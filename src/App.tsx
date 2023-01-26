64a299c12d390937d1755ad9802890e4c13f848d
Wayne Osborn <jacobwayne1513@aol.com>
2023-01-26 16:25:55 -0500

    not just a basic react guy surely

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
+import alertTriangle from './alert-triangle.svg';
+import styled from 'styled-components'
 import './App.css';
+
+const WarningImg = styled.img`
+  width: 40vmin;
+`
 
 const App = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <WarningImg src={alertTriangle} className="App-logo" alt="logo" />
+        <h1>Under Contruction</h1>
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Sorry, Wayne's working hard to build something great
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
